41fd44151414ac0569fae1ed69e72956071b1166
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-11 14:17:18 +0100

    Working on the contact us page

---
+++ src/Components/ContactUs/ContactUsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactUsPage = () => {
+  return (
+    <>
+        <div className="ContactUsPage">
+            
+        </div>
+    </>
+  )
+}
+
+export default ContactUsPage
