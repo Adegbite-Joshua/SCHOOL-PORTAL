adcccd2decd819c82dd6b0b68933289a8d8e7354
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-03 22:30:58 +0100

    Working on fetching the students resources

--- src/Components/Subjects/ResourcesContainer.jsx
+++ src/Components/Subjects/ResourcesContainer.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useEffect}from 'react'
 import SubjectResource from './SubjectResource'
 
 const ResourcesContainer = () => {
+  useEffect(({subjectIndex})=>{
+    if(subjectIndex){
+      let endpoint = 'http://localhost:7777/student/fetchsubjectresources'
+      axios.post(endpoint, payload)
+    }
+  }, [fetchSubjectResources])
   return (
     <>
         <SubjectResource/>
