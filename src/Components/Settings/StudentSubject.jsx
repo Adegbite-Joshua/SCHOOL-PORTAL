c3f9b44971d62f4169927471f05bd19bbc68dfc8
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-04 23:27:27 +0100

    Working on showing the students added subjects and unadded subjects

--- src/Components/Settings/StudentSubject.jsx
+++ src/Components/Settings/StudentSubject.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import subjects from '../../subjectArray';
 import Subject from './Subject'
 
 
 const ErrorPage = () => {
     // document.querySelector("title").innerText = `404 - Error Page`
     let studentInfo = useSelector((state)=>state.studentInformation.studentInformation);
-    let su
+    let addedSubjects = [];
+    studentInfo.subjects.map((subject, index)=>{
+        addedSubjects.push({subjectIndex: subject.subjectIndex, index:index});
+    })
+    let unAddedSubjects = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+    // for (let i = 0; i < subjects.length; i++) {
+    //     console.log();
+    //     // addedSubjects.map((subject, index)=>{
+    //     //     let found = [];
+    //     //     if (subject.subjectIndex!=i && !unAddedSubjects.includes(i)) {
+    //     //         unAddedSubjects.push(i)
+    //     //     }
+    //     // })
+    // }
+    // console.log(addedSubjects);
+    // addedSubjects.map((subject, index)=>{
+    //     if (unAddedSubjects.includes(subject.subjectIndex)) {
+    //         unAddedSubjects.splice(subject.subjectIndex,1)
+    //     }
+    // })
+    for (let i = 0; i < 11; i++) {
+        
+        
+    }
+    console.log(addedSubjects);
+    console.log(unAddedSubjects);
   return (
     <>
         <div className='w-100 px-4'>
