efa9747737c1729f18786de500ace127b2bc22c6
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-08 17:29:36 +0100

    Working on adding React advancement update showing the active link

--- src/Components/StudentNav/StudentSideNav.jsx
+++ src/Components/StudentNav/StudentSideNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './style.scss'
 
 const StudentSideNav = () => {
@@ -11,13 +11,13 @@ const StudentSideNav = () => {
         <div id="mySidenav" className="sidenav blue600 text-center d-flex w-100 px-5">
             <img src="/vite.svg" style={{height: '60px', width: '60px'}} className='' alt="" />
             <h3 className='text-light'>PROADE SCHOOL</h3>
-            <Link to='/dashboard'><span id='icon'><i className='fas fa-home '></i><span>Home</span></span></Link>
-            <Link to='/inbox'><span id='icon'><i className='fas fa-inbox '></i><span>Inbox</span></span></Link>
-            <Link to='/subjects'><span id='icon'><i className='fas fa-book '></i><span>Subjects</span></span></Link>
-            <Link to='/announcement'><span id='icon'><i className='fas fa-bell '></i><span>Announcements</span></span></Link>
-            <Link to='/activities'><span id='icon'><i className='fas fa-calendar '></i><span>Activities</span></span></Link>
-            <Link to='/settings'><span id='icon'><i className='fas fa-gear '></i><span>Settings</span></span></Link>
-            <Link to='/'><span id='icon'><i className='fas fa-sign-out '></i><span>Sign Out</span></span></Link>    
+            <NavLink to='/dashboard'><span id='icon'><i className='fas fa-home '></i><span>Home</span></span></NavLink>
+            <NavLink to='/inbox'><span id='icon'><i className='fas fa-inbox '></i><span>Inbox</span></span></NavLink>
+            <NavLink to='/subjects'><span id='icon'><i className='fas fa-book '></i><span>Subjects</span></span></NavLink>
+            <NavLink to='/announcement'><span id='icon'><i className='fas fa-bell '></i><span>Announcements</span></span></NavLink>
+            <NavLink to='/activities'><span id='icon'><i className='fas fa-calendar '></i><span>Activities</span></span></NavLink>
+            <NavLink to='/settings'><span id='icon'><i className='fas fa-gear '></i><span>Settings</span></span></NavLink>
+            <NavLink to='/'><span id='icon'><i className='fas fa-sign-out '></i><span>Sign Out</span></span></NavLink>    
             <span id='openPop' className='p-2 rounded-3 border border-2 border-light d-md-none' onClick={openNavPop}><i className='fas fa-bars text-light'></i></span>
         </div>
     </>
