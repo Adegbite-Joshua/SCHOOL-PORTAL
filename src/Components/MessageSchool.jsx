194133980cafeb4ea6754bf93ee14f0051696186
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-15 21:57:04 +0100

    Worked on using snackbar to display error while messaging the school

--- src/Components/MessageSchool.jsx
+++ src/Components/MessageSchool.jsx
@@ -1,13 +1,24 @@
 import axios from 'axios';
 import { useFormik } from 'formik';
-import React from 'react'
+import React, { useState } from 'react'
+import SnackBar from '../SnackBar';
 
 const MessageSchool = () => {
+    const [snackbarMessage, setsnackbarMessage] = useState('');
+    const [snackbarType, setsnackbarType] = useState('');
     const openForm =()=> {
         document.getElementById("myForm1").style.display = "block";
     }
     const closeForm =()=> {
         document.getElementById("myForm1").style.display = "none";
+    }
+
+    const showSnackBar = () => {
+        // Get the snackbar DIV
+        var x = document.getElementById("snackbarContainer");
+        x.className = "show";
+      
+        setTimeout(()=>{ x.className = x.className.replace("show", ""); }, 3000);
     }
 
     const formik = useFormik({
@@ -19,16 +30,28 @@ const MessageSchool = () => {
         onSubmit: (values)=>{
             console.log(values);
             let messageEndpoint = 'http://localhost:7777/student/messageschool'
-            axios.post(messageEndpoint, {values})
+            axios.post(messageEndpoint, values)
             .then((response)=>{
                 if (response.status == 200) {
                    alert('Message sent') 
+                   setsnackbarType('info')
+                   setsnackbarMessage('Message successfully sent')
+                   closeForm()
+                   showSnackBar()
                 } else{
-                    alert('errpr in sending message')
+                    setsnackbarType('error')
+                    setsnackbarMessage('An error ocurred, please try again')
+                    closeForm()
+                    showSnackBar()
+                    // alert('errpr in sending message')
                 }
             })
             .catch((error)=>{
-                console.log(error);
+                setsnackbarType('error')
+                setsnackbarMessage('An error ocurred, please try again')
+                closeForm()
+                showSnackBar()
+                // console.log(error);
             })
         }
     })
@@ -39,16 +62,17 @@ const MessageSchool = () => {
         <form className="form-container1" onSubmit={formik.handleSubmit}>
             <h1>Chat</h1>
             <small>Having any question, suggestion or issue?</small>
-            <label for="msg"><b>Message Hope Academy Admin</b></label>
+            <label for="message"><b>Message Hope Academy Admin</b></label>
             <label htmlFor="name">Name</label>
-            <input type="text" name='name' {...formik.getFieldProps('name')} placeholder='Your Name' className='form-control mt-1' />
+            <input type="text" name='name' {...formik.getFieldProps('name')} placeholder='Your Name ' required className='form-control mt-1' />
             <label htmlFor="email">Email</label>
-            <input type="email" name='email' {...formik.getFieldProps('email')} placeholder='Your email' className='form-control mt-1' />
-            <textarea placeholder="Type message.." {...formik.getFieldProps('msg')} className='form-control mt-1' name="msg" required></textarea>
+            <input type="email" name='email' {...formik.getFieldProps('email')} placeholder='Your email' required className='form-control mt-1' />
+            <textarea placeholder="Type message.." {...formik.getFieldProps('message')} className='form-control mt-1' name="message" required></textarea>
             <button type="submit" className="btn1"><i className='fas fa-send'></i></button>
             <button type="button" className="btn1 cancel1 btn rounded-pill" onClick={closeForm}>Close</button>
         </form>
         </div>
+        <div id='snackbarContainer'><SnackBar body={snackbarMessage} type={snackbarType}/></div>
     </>
   )
 }
