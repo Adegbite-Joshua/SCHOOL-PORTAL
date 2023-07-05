da703058b3f4254531650649a4f56e54d705125d
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-07-05 18:41:58 +0100

    Working on sending request to and receiving from the backend

--- src/Components/Announcements/StudentAnnouncement.jsx
+++ src/Components/Announcements/StudentAnnouncement.jsx
@@ -9,21 +9,30 @@ const StudentAnnouncement = () => {
     document.querySelector("title").innerText = `Announcement`
     let studentInfo = useSelector((state)=>state.studentInformation);
     let socket = useRef()
+    socket.current = socketClient('http://localhost:7777/student')
     const validateToken =()=>{
-      socket.current.emit('student/validate_token', 'hello')
+      socket.current.emit('validate_token', 'hello')
     }
     const recieveSocket =()=>{
-      if(socket.current){
+      console.log('recieving');
         socket.current.on('back', (message)=>{
-          console.log(message)
+          console.log(message);
         })
-      }
     }
+
     useEffect(()=>{
-      socket.current = socketClient('http://localhost:7777/')
-      validateToken()
-      recieveSocket()
+      socket.current.on('connect', ()=>{
+        console.log('Student connected');
+        validateToken()
+      })
+    }, [])
+
+    useEffect(()=>{
+      // if(socket.current){
+        recieveSocket()
+      // }
     }, [socket])
+   
   return (
     <>
         <div className="d-flex">
